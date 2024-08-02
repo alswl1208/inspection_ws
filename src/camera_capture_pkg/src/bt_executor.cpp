b309066e46eae7ae160797b2038ca9e84de95d56
minji <alswl1208@kau.kr>
2024-08-02 14:40:11 +0900

    Removed unnecessary files

--- src/camera_capture_pkg/src/bt_executor.cpp
+++ src/camera_capture_pkg/src/bt_executor.cpp
@@ -1,11 +1,30 @@
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <py_trees_ros/trees.hpp>
+#include <py_trees_ros/conversions.hpp>
+#include <py_trees/behaviours.hpp>
+#include <py_trees/blackboard.hpp>
+#include <py_trees/composites.hpp>
+#include <py_trees/decorators.hpp>
 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-  rclcpp::init(argc, argv);
-  auto node = std::make_shared<rclcpp::Node>("bt_executor");
-  RCLCPP_INFO(node->get_logger(), "BT Executor node has been started.");
-  rclcpp::spin(node);
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::init(argc, argv);
+
+    auto node = rclcpp::Node::make_shared("bt_executor");
+
+    // Create a tree
+    py_trees::BehaviorTree tree;
+
+    // Load the tree structure from the XML file
+    std::string tree_file_path = "/home/minji/inspection_ws/src/camera_capture_pkg/inspection_tree.xml"; // Change to your path
+    py_trees::xml::Document document;
+    document.load(tree_file_path);
+    tree = py_trees_ros::from_document(document, node);
+
+    // Run the tree
+    py_trees::ros::Tree ros_tree(node, tree);
+    rclcpp::spin(node);
+
+    rclcpp::shutdown();
+    return 0;
 }
